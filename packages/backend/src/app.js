ed82671121eb5974f37ea89754f5955359367f0e
pedro <pedro092692@gmail.com>
2025-05-04 09:46:55 +0200

    first commit

---
+++ packages/backend/src/app.js
@@ -0,0 +1,22 @@
+import express from 'express'
+
+class Server {
+    constructor(){
+        this.app = express()
+        this.port = 3000
+        this.routes()
+
+    }
+
+    routes(){
+        this.app.get('/', (req, res) => res.send('Hello world'))
+    }
+
+    start(){
+        this.app.listen(this.port, () => {
+            console.log(`Server is running at http://127.0.0.1:${this.port}`)
+        })
+    }
+}
+
+export default Server
---
+++ packages/backend/src/index.js
@@ -0,0 +1,4 @@
+import Server from './app.js'
+
+const app = new Server()
+app.start()
