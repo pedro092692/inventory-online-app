0f077479c624348042ef11dd99dc5733e5466159
pedro <pedro092692@gmail.com>
2025-05-23 09:41:11 +0200

    update config file for sequelize-cli

--- packages/backend/src/config/config.js
+++ packages/backend/src/config/config.js
@@ -1,1 +1,16 @@
+/* eslint-disable no-undef */
+require('dotenv').config()
 
+const dbConfig = {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.HOST,
+    port: process.env.PORT,
+    dialect: "postgres"
+}
+
+module.exports = {
+    development: dbConfig,
+    production: dbConfig,
+}
