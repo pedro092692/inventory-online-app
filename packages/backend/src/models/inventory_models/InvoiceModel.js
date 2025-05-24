782ca76107a0fb177d1528dee888d866515823c3
pedro <pedro092692@gmail.com>
2025-05-24 14:25:03 +0200

    create invoice model

---
+++ packages/backend/src/models/inventory_models/InvoiceModel.js
@@ -0,0 +1,65 @@
+import { DataTypes, Model } from "sequelize"
+
+class Invoice extends Model {
+    // model relations
+}
+
+function initializeInvoice(sequelize) {
+    Invoice.init(
+        {
+            id: {
+                 type: DataTypes.INTEGER,
+                 autoIncrement: true,
+                 primaryKey: true
+             },
+
+            date: {
+                type: DataTypes.DATE,
+                defaultValue: new Date(),
+                allowNull: false,
+                validate: {
+                    isDate: {
+                        msg: "A valid date is required."
+                    }
+                }
+            },
+
+            customer_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: "customers",
+                    key: "id"
+                }
+            },
+
+            seller_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: "sellers",
+                    key: "id"
+                }
+            },
+
+            total: {
+                type: DataTypes.DECIMAL(10, 2),
+                allowNull: false,
+                defaultValue: 0.00,
+                validate: {
+                    isNumeric: {
+                        msg: "A valid number is required."
+                    }
+                }
+            }
+        },
+        {
+            sequelize,
+            modelName: "Invoice",
+            tableName: "invoices",
+            timestamps: false,
+        }
+    )
+}
+
+export { initializeInvoice, Invoice } 
