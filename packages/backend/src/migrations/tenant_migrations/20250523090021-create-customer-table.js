d2d68e1a2c86376f8e4522d7a18c0b1da60c7f60
pedro <pedro092692@gmail.com>
2025-05-24 16:47:17 +0200

    create util to make test migrations and add customers table into test_schema

--- packages/backend/src/migrations/tenant_migrations/20250523090021-create-customer-table.js
+++ packages/backend/src/migrations/tenant_migrations/20250523090021-create-customer-table.js
@@ -1,16 +1,51 @@
 export default {
-  async up (queryInterface, Sequelize, schema ) {
+  up: async (queryInterface, Sequelize, schema) => {
     await queryInterface.createTable(
       'customers',
       {
-        id:{
-          type: Sequelize.INTERGER
-        }
+        id: {
+           type: Sequelize.INTEGER,
+           autoIncrement: true,
+           primaryKey: true
+         },
+
+         id_numer: {
+          type: Sequelize.INTEGER, // int for Venezuelan id
+          allowNull: false, 
+          validate: {
+            notEmpty: true
+          }
+         }, 
+
+         name: {
+          type: Sequelize.STRING,
+          allowNull: false, 
+          validate: {
+            notEmpty: true,
+          }
+         },
+         
+         phone: {
+          type: Sequelize.STRING,
+          allowNull: false,
+          defaultValue: "+58424000000", // phone number for venezuela 
+          validate: {
+            len: {
+              args: [12]
+            }
+          }
+         }
+      },
+      {
+        schema
       }
     )
   },
 
-  async down (queryInterface, Sequelize) {
-    
+  down: async (queryInterface, Sequelize, schema) => {
+    await queryInterface.dropTable({
+      tableName: 'customers',
+      schema
+    })
   }
 };
---
+++ packages/backend/src/utils/testMigrations.js
@@ -0,0 +1,66 @@
+import { Sequelize } from 'sequelize'
+import { Umzug, SequelizeStorage } from "umzug"
+import dotenv from "dotenv"
+import path from "path"
+import { fileURLToPath, pathToFileURL } from "url"
+import process from "process"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const envPath = path.resolve(__dirname, "../../.env")
+dotenv.config({ path: envPath })
+
+async function testMigration() {
+    // sequelize new instance
+    const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: 'postgres',
+        logging: false,
+        schema: "test_schema"  // set test schema for testing.
+    })
+
+    // queryInterface for migration 
+    const queryInterface = sequelize.getQueryInterface()
+
+    // umzug instance 
+    const umzug = new Umzug({
+        migrations: {
+            glob: "../migrations/tenant_migrations/*.js",
+            resolve: ({ name, path, context }) => {
+                return {
+                    name,
+                    up: async () => {
+                        const migrationPath = pathToFileURL(path)
+                        const migration = ( await import(migrationPath)).default
+                        migration.up(context.queryInterface, Sequelize, context.schema)
+                    }
+                }
+            }
+        },
+        context: {
+            sequelize,
+            queryInterface,
+            schema: "test_schema"
+        },
+        logger: console,
+        storage: new SequelizeStorage({
+            sequelize,
+            tableName: "SequelizeMeta",
+            schema: "test_schema"
+        })
+    })
+
+    const new_migrations = await umzug.pending()
+    if(new_migrations.length > 0) {
+        await umzug.up() // execute migrations
+    }
+
+    console.log("Migrations were executed successfully.")
+
+}
+
+await testMigration().then( () => process.exit(0) ).catch(error => {
+    console.log("Error executing migrations:", error)
+    process.exit(1)
+})
