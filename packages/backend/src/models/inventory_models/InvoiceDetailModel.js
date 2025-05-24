e2097bedcc5407eac97966bbd484420421d33195
pedro <pedro092692@gmail.com>
2025-05-24 14:36:51 +0200

    create InvoiceDetail model

---
+++ packages/backend/src/models/inventory_models/InvoiceDetailModel.js
@@ -0,0 +1,66 @@
+import { DataTypes, Model } from "sequelize"
+
+
+class InvoiceDetail extends Model {
+    // model ralations
+}
+
+function initializeInvoiceDetail(sequelize) {
+    InvoiceDetail.init(
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+
+            invoice_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: "invoices",
+                    key: "id"
+                }
+            },
+
+            product_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: "products",
+                    key: "id"
+                }
+            },
+
+            quantity: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                defaultValue: 1,
+                validate: {
+                    isNumeric: {
+                        msg: "A valid number is required."
+                    }
+                }
+            },
+
+            unit_price: {
+                type: DataTypes.DECIMAL(10, 2),
+                allowNull: false,
+                validate: {
+                    isNumeric: {
+                        msg: "A valid price is required."
+                    }
+                }
+            }
+
+        },
+        {
+            sequelize,
+            modelName: "InvoiceDetail",
+            tableNamme: "InvoiceDetails",
+            timestamps: false
+        }
+    )
+}
+
+export { initializeInvoiceDetail, InvoiceDetail }
