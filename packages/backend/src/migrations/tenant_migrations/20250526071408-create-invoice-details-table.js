fcedd2080c140c177a1f82ce3fa812c283b8c112
pedro <pedro092692@gmail.com>
2025-05-26 09:25:01 +0200

    create migration for invoice details into test_schema

---
+++ packages/backend/src/migrations/tenant_migrations/20250526071408-create-invoice-details-table.js
@@ -0,0 +1,61 @@
+export default {
+  up: async (queryInterface, Sequelize, schema) => {
+    await queryInterface.createTable(
+      "invoices_details",
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          autoIncrement: true,
+          primaryKey: true
+        },
+
+        invoice_id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: "invoices",
+            key: "id"
+          },
+          onUpdate: "CASCADE",
+        },
+
+        product_id: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: "products",
+            key: "id"
+          },
+          onUpdate: "CASCADE"
+        },
+
+        quantity: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          defaultValue: 1,
+          validate: {
+            isNumeric: true
+          }
+        },
+
+        unit_price: {
+          type: Sequelize.DECIMAL(10, 2),
+          allowNull: false,
+          validate: {
+            isNumeric: true
+          }
+        }
+      },
+      {
+        schema
+      }
+    )
+  },
+
+  down: async (queryInterface, Sequelize, schema) => {
+    await queryInterface.dropTable({
+      tableName: "invoices_details",
+      schema
+    })
+  }
+};
