6954a8839592f8fc0142c1fa468ad20c12faf124
pedro <pedro092692@gmail.com>
2025-05-26 17:26:13 +0200

    crete seeder for sellers

--- packages/backend/src/seeders/20250526075340-seed-customers.js
+++ packages/backend/src/seeders/20250526075340-seed-customers.js
@@ -11,6 +11,18 @@ export default {
           id_number: 1000000, // Venezuelan ID number
           name: "John Doe",
           phone: "+58424000000", // Default phone number for Venezuela
+        },
+        {
+          id: 2,
+          id_number: 2000000, // Another Venezuelan ID number
+          name: "Jane Smith",
+          phone: "+58424000001", // Another phone number for Venezuela
+        },
+        {
+          id: 3,
+          id_number: 3000000, // Yet another Venezuelan ID number
+          name: "Carlos Perez",
+          phone: "+58424000002", // Yet another phone number for Venezuela
         }
       ]
     )
---
+++ packages/backend/src/seeders/20250526151740-seed-seller.js
@@ -0,0 +1,45 @@
+export default {
+  up: async (queryInterface, Sequelize, schema) => {
+    await queryInterface.bulkInsert(
+      {
+          tableName: "sellers",
+          schema: schema
+      },
+      [
+        {
+          id: 1,
+          id_number: 5000000, // Venezuelan ID number
+          name: "Pedro",
+          last_name: "Bastidas",
+          address: "123 Main St, Caracas, Venezuela", // Example address
+        },
+        {
+          id: 2,
+          id_number: 6000000, // Another Venezuelan ID number
+          name: "Andrea",
+          last_name: "Gonzalez",
+          address: "456 Elm St, Maracaibo, Venezuela", // Another example address
+        },
+        {
+          id: 3,
+          id_number: 6000000, // Yet another Venezuelan ID number
+          name: "Xavier",
+          last_name: "Martinez",
+          address: "789 Oak St, Valencia, Venezuela", // Yet another example address
+        }
+      ]
+    )
+  },
+
+  down: async (queryInterface, Sequelize, schema) => {
+    await queryInterface.bulkDelete(
+      {
+        tableName: "sellers",
+        schema: schema
+      },
+      {
+        id: [1, 2, 3] // Delete all seeded sellers
+      }
+    );
+  }
+};
