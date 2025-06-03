e0b0dd929a859a2cb2f599a29b928c6d1a3a1c9d
pedro <pedro092692@gmail.com>
2025-06-03 15:50:22 +0200

    create get product by id funcion in product service

--- packages/backend/src/services/ProductService.js
+++ packages/backend/src/services/ProductService.js
@@ -1,17 +1,29 @@
+import ServiceErrorHandler from "../errors/ServiceErrorHandler.js"
+
 class ProductService{
+    // instance of error handler
+    #error = new ServiceErrorHandler()
+
     constructor(model) {
         this.Product = model
+        this.#error
     }
 
-    async getAllProducts() {
-        try {
-            return await this.Product.findAll({
-                limit: 10,
-                offset: 0
+    getAllProducts(limit=10, offset=0) {
+        return this.#error.handler("Read All Products", async () => {
+            const products = await this.Product.findAll({
+                limit: limit,
+                offset: offset
             })
-        } catch(error) {
-            throw new Error(`Error fetching products: ${error.message}`)
-        }
+            return products
+        })    
+    }
+
+    getProduct(id) {
+        return this.#error.handler(["Read Product", id, "Product"], async () => {
+            const product = await this.Product.findByPk(id)
+            return product
+        })
     }
 }
 
