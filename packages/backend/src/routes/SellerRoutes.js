e89d3bf2658be485eb1259a3feacbeac30ec9063
pedro <pedro092692@gmail.com>
2025-06-05 15:19:23 +0200

    add update seller patch method on seller routes

--- packages/backend/src/routes/SellerRoutes.js
+++ packages/backend/src/routes/SellerRoutes.js
@@ -13,6 +13,7 @@ class SellerRoutes{
         this.router.get("/all", (req, res) => new SellerController(Seller).allSeller(req, res))
         this.router.get("/:id", (req, res) => new SellerController(Seller).getSeller(req, res))
         this.router.post("/", (req, res) => new SellerController(Seller).createSeller(req, res))
+        this.router.patch("/:id", (req, res) => new SellerController(Seller).updateSeller(req, res))
     }
 }
 
