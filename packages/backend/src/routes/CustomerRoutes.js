cef6de6c2f8f91f5c13c07cfbfd11e304149f800
pedro <pedro092692@gmail.com>
2025-06-05 19:32:20 +0200

    add update seller patch method on customer routes

--- packages/backend/src/routes/CustomerRoutes.js
+++ packages/backend/src/routes/CustomerRoutes.js
@@ -14,6 +14,7 @@ class CustomerRoutes {
         this.router.get("/all", (req, res) => new CustomerController(Customer).allCustomers(req, res))
         this.router.get("/:id", (req, res) => new CustomerController(Customer).getCustomerById(req, res))
         this.router.post("/", (req, res) => new CustomerController(Customer).createCustomer(req, res))
+        this.router.patch("/:id", (req, res) => new CustomerController(Customer).updateCustomer(req, res))
     }
 }
 
