4a2fb69226b177bf2259d8c2cc84a5c94fdeb759
pedro <pedro092692@gmail.com>
2025-06-11 16:39:19 +0200

    Add JSDocs to: createUser, getAllUsers, getUser, updateUser, deleteUser functions in user controller

--- packages/backend/src/Controllers/admin/UserController.js
+++ packages/backend/src/Controllers/admin/UserController.js
@@ -10,24 +10,51 @@ class UserController {
         this.#error
     }
 
+    /**
+     * Creates a new user.
+     * @param {Object} req - request object containing user details in the body
+     * @param {Object} res - response object to send the created user
+     * @throws {ServiceError} - throws an error if the user could not be created
+     * @returns {Promise<void>} - returns the created user in the response
+     */
     createUser = this.#error.handler( async(req, res) => {
         const { email, password, roleId } = req.body
         const user = await this.User.createUser(email, password, roleId)
         res.status(201).json(user)
     })
     
-
+    /**
+     * Retrieves all users.
+     * @param {Object} req - request object
+     * @param {Object} res - response object to send the list of users
+     * @throws {ServiceError} - throws an error if the users could not be retrieved
+     * @returns {Promise<void>} - returns the list of users in the response
+     */
     getAllUsers = this.#error.handler( async(req, res) => {
         const users = await this.User.getAllUser()
         res.status(200).json(users)
     })
 
+    /**
+     * Retrieves a user by their ID.
+     * @param {Object} req - request object containing the user ID in the params
+     * @param {Object} res - response object to send the user details
+     * @throws {ServiceError} - throws an error if the user could not be found
+     * @returns {Promise<void>} - returns the user details in the response
+     */
     getUser = this.#error.handler( async(req, res) => {
         const { id } = req.params
         const user = await this.User.getUser(id)
         res.status(200).json(user)
     })
 
+    /**
+     * Updates a user by their ID.
+     * @param {Object} req - request object containing the user ID and updates in the body
+     * @param {Object} res - response object to send the updated user
+     * @throws {ServiceError} - throws an error if the user could not be updated
+     * @returns {Promise<void>} - returns the updated user in the response
+     */
     updateUser = this.#error.handler( async(req, res) => {
         const userId = req.body.userId
         const updates = req.body.updates
@@ -35,6 +62,13 @@ class UserController {
         res.status(200).json(updatedUser)
     })
 
+    /**
+     * Deletes a user by their ID.
+     * @param {Object} req - request object containing the user ID in the body
+     * @param {Object} res - response object to send the deletion confirmation
+     * @throws {ServiceError} - throws an error if the user could not be deleted
+     * @returns {Promise<void>} - returns a confirmation message in the response
+     */
     deleteUser = this.#error.handler( async(req, res) => {
         const userId = req.body.userId
         // delete user 
