0f20a7acf41172ef721898b45b1c676d9bf3ce57
pedro <pedro092692@gmail.com>
2025-06-11 16:56:34 +0200

    Add JSDocs to createProduct, allProducts, getProduct, updateProduct, deleteProduct functions in product controller

--- packages/backend/src/Controllers/ProductController.js
+++ packages/backend/src/Controllers/ProductController.js
@@ -10,23 +10,51 @@ class ProductController{
         this.#error
     }
 
+    /**
+     * Creates a new product.
+     * @param {Object} req - request object containing product details in the body
+     * @param {Object} res - response object to send the created product
+     * @throws {ServiceError} - throws an error if the product could not be created
+     * @returns {Promise<void>} - returns the created product in the response
+     */
     createProduct = this.#error.handler( async(req, res) => {
         const { barcode, name, purchase_price, selling_price, stock } = req.body
         const product = await this.ProductService.createProduct(barcode, name, purchase_price, selling_price, stock)
         res.status(201).json(product)
     })
 
+    /**
+     * Retrieves all products.
+     * @param {Object} req - request object
+     * @param {Object} res - response object to send the list of products
+     * @throws {ServiceError} - throws an error if the products could not be retrieved
+     * @returns {Promise<void>} - returns the list of products in the response
+     */
     allProducts = this.#error.handler( async(req, res) => {
         const products = await this.ProductService.getAllProducts()
         res.status(200).json(products)
     })
 
+    /**
+     * Retrieves a product by its ID.
+     * @param {Object} req - request object containing the product ID in the params
+     * @param {Object} res - response object to send the product details
+     * @throws {ServiceError} - throws an error if the product could not be found
+     * @returns {Promise<void>} - returns the product details in the response
+     */
     getProduct = this.#error.handler( async(req, res) => {
         const { id } = req.params
         const product = await this.ProductService.getProduct(id)
         res.status(200).json(product)
     })
 
+    /**
+     * Updates a product by its ID.
+     * @param {Object} req - request object containing the product ID and updates in the body
+     * @param {Object} res - response object to send the updated product
+     * @throws {ServiceError} - throws an error if the product could not be updated
+     * @returns {Promise<void>} - returns the updated product in the response
+     */
     updateProduct = this.#error.handler( async(req, res) => {
         const { id } = req.params
         const updates = req.body
@@ -34,6 +62,13 @@ class ProductController{
         res.status(200).json(updatedProduct)
     })
 
+    /**
+     * Deletes a product by its ID.
+     * @param {Object} req - request object containing the product ID in the body
+     * @param {Object} res - response object to send a success status
+     * @throws {ServiceError} - throws an error if the product could not be deleted
+     * @returns {Promise<void>} - returns a success status in the response
+     */
     deleteProduct = this.#error.handler( async(req, res) => {
         const productId = req.body.productId 
         // delete product
