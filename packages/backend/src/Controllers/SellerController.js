0383c0ef0779134fd1ff24743aa61925b2d0eef0
pedro <pedro092692@gmail.com>
2025-06-11 17:02:11 +0200

    Add JSDocs to createSeller, allSeller, getSeller, updateSeller, deleteSeller function in seller controller

--- packages/backend/src/Controllers/SellerController.js
+++ packages/backend/src/Controllers/SellerController.js
@@ -10,24 +10,55 @@ class SellerController {
         this.#error
     }
 
+    /**
+     * Creates a new seller.
+     * @param {Object} req - request object containing seller details in the body
+     * @param {Object} res - response object to send the created seller
+     * @throws {ServiceError} - throws an error if the seller could not be created
+     * @returns {Promise<void>} - returns the created seller in the response
+     */
     createSeller = this.#error.handler( async(req, res) => {
         const { id_number, name, last_name, address } = req.body
         const seller = await this.sellerService.createSeller(id_number, name, last_name, address)
         res.status(201).json(seller)
     })
 
-
+    /**
+     * Retrieves all sellers.
+     * @param {Object} req - request object
+     * @param {Object} res - response object to send the list of sellers
+     * @throws {ServiceError} - throws an error if the sellers could not be retrieved
+     * @returns {Promise<void>} - returns the list of sellers in the response
+     */
     allSeller = this.#error.handler(async(req, res) => {
         const sellers = await this.sellerService.getAllSellers()
         res.status(200).json(sellers)
     })
 
+    /**
+     * Retrieves a seller by their ID.
+     * @param {Object} req - request object containing the seller ID in the params
+     * @param {Object} res - response object to send the seller details
+     * @throws {ServiceError} - throws an error if the seller could not be found
+     * @returns {Promise<void>} - returns the seller details in the response
+     */
     getSeller = this.#error.handler( async(req, res) => {
         const { id } = req.params
         const seller = await this.sellerService.getSeller(id)
         res.status(200).json(seller)
     })
 
+    /**
+     * Updates a seller by their ID.
+     * @param {Object} req - request object containing the seller ID in the params and updates in the body
+     * @param {Number} req.params.id_number -new ID number of the seller
+     * @param {String} req.body.name - new name of the seller
+     * @param {String} req.body.last_name - new last name of the seller
+     * @param {String} req.body.address - new address of the seller
+     * @param {Object} res - response object to send the updated seller
+     * @throws {ServiceError} - throws an error if the seller could not be updated
+     * @returns {Promise<void>} - returns the updated seller in the response
+     */
     updateSeller = this.#error.handler( async(req, res) => {
         const { id } = req.params
         const updates = req.body 
@@ -35,6 +66,13 @@ class SellerController {
         res.status(200).json(updatedSeller)
     })
 
+    /**
+     * Deletes a seller by their ID.
+     * @param {Object} req - request object containing the seller ID in the body
+     * @param {Object} res - response object to send a success status
+     * @throws {ServiceError} - throws an error if the seller could not be deleted
+     * @returns {Promise<void>} - returns a success status in the response
+     */
     deleteSeller = this.#error.handler( async(req, res) => {
         const sellerId = req.body.sellerId
         // delete seller 
