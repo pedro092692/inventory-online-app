9e28b39f32c86af89e381b328a0dbb0a6f317f9f
pedro <pedro092692@gmail.com>
2025-06-12 15:40:45 +0200

    add getPyamentMethod function in payment method controller

--- packages/backend/src/Controllers/PaymentMethodController.js
+++ packages/backend/src/Controllers/PaymentMethodController.js
@@ -22,4 +22,17 @@ class PaymentMethodController {
         const newPaymentMethod = await this.PaymentMethod.cretePaymentMethod(name, currency)
         res.status(201).json(newPaymentMethod)
     })
+
+    /**
+     * Retrieves a payment method by its ID.
+     * @param {Object} req - request object containing the payment method ID in the params
+     * @param {Object} res - response object to send the payment method details
+     * @throws {ServiceError} - throws an error if the payment method could not be found
+     * @returns {Promise<void>} - returns the payment method details in the response
+     */
+    getPaymentMethod = this.#error.handler( async(req, res) => {
+        const { id } = req.params
+        const paymentMethod = await this.PaymentMethod.getPaymentMethod(id)
+        res.status(200).json(paymentMethod)
+    })
 }
