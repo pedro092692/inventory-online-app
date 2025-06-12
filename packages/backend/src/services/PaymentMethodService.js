1e89fdaaf5d57f0e78aadfd2cd348f8befe02f42
pedro <pedro092692@gmail.com>
2025-06-12 15:00:28 +0200

    add getPaymentMethod function in payment method service

--- packages/backend/src/services/PaymentMethodService.js
+++ packages/backend/src/services/PaymentMethodService.js
@@ -15,7 +15,7 @@ class PaymentMethodService {
      * 
      * @param {String} name - Name of payment method E.g bank tranfer 
      * @param {String} currency - Name of currency of payment method E.g Dollar  
-     * @returns {Promise<object>} - return a object with data of new payment method
+     * @returns {Promise<object>} - A object with data of new payment method
      * @throws {Error} - Throws an error if create payment fails.
      */
     cretePaymentMethod(name, currency) {
@@ -26,6 +26,24 @@ class PaymentMethodService {
             })
 
             return newPayment
+        })
+    }
+
+    /**
+     * 
+     * @param {Number} id - Id of the payment method 
+     * @returns {Promise<object}  A object with data of new payment method
+     * @throws {Error} - Throws an error if get payment method fails.
+     */
+    getPaymentMethod(id) {
+        return this.#error.handler(["Read Payment Method", id, "Payment"], async() => {
+            const paymentMethod = await this.PaymentMethod.findByPk(id)
+            
+            if(!paymentMethod) {
+                throw new NotFoundError()
+            }
+
+            return paymentMethod
         })
     }
 }
