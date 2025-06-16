fcbe198dbcca286b9fa1c64e060314ca9ba71cf2
pedro <pedro092692@gmail.com>
2025-06-16 20:59:04 +0200

    add creataePaymentInvoiceDetail funcion in payment detail controller file

---
+++ packages/backend/src/Controllers/PaymentInvoiceDetailController.js
@@ -0,0 +1,27 @@
+import ControllerErrorHandler from "../errors/controllerErrorHandler"
+import PayInvoiceService from "../services/PayInvoiceService"
+
+class PayInvoiceController {
+    // Error hanlder instance 
+    #error = new ControllerErrorHandler()
+
+    constructor(model) {
+        this.PayInvoice = new PayInvoiceService(model)
+        this.#error
+    }
+
+    /**
+     * Creates new payment detail for a invoice
+     * @param {Object} req - request object containing payment detail in the body
+     * @param {Object} res - response object to send the created payment detail info
+     * @throws {ServiceError} - throws an error if the payment detail could not be created
+     * @returns {Promise<void>} - returns the created payment detail in the response
+     */
+    createPaymentInvoiceDetail = this.#error.handler( async(req, res) => {
+        const { invoice_id, payment_id, amount } = req.body
+        const newPaymentDetail = await this.PayInvoice.createPaymentDetail(invoice_id, payment_id, amount)
+        res.status(201).json(newPaymentDetail)
+    })
+}
+
+export default PayInvoiceController
