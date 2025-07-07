c277418a0d8e30b3bb77d2e520f323efbcb1c9c1
pedro <pedro092692@gmail.com>
2025-07-07 16:46:22 +0200

    create dollar model file

---
+++ packages/backend/src/models/inventory_models/DollarModel.js
@@ -0,0 +1,47 @@
+import { DataTypes, Model } from "sequelize"
+
+class Dollar extends Model {
+    // model relations
+}
+
+function initializeDollar(sequelize) {
+    Dollar.init(
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true
+            },
+
+            value: {
+                type: DataTypes.DECIMAL(10, 2),
+                allowNull: false,
+                validate: {
+                    isNumeric: {
+                         msg: "A valid number is required."
+                    }
+                }
+            },
+
+            date: {
+                type: DataTypes.DATE,
+                allowNull: false,
+                defaultValue: new Date(),
+                validate: {
+                    isDate: {
+                        msg: "A valid date is required."
+                    }
+                }
+            }
+        },
+        {
+            sequelize,
+            modelName: "Dollar",
+            tableName: "dollar-value",
+            timestamps: false,
+            schema: "test_schema" // only for test purposes
+        }
+    )
+}
+
+export { initializeDollar, Dollar }
