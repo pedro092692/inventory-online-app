9548ed5a6daab28777eb4352b4fc41cd51704838
pedro <pedro092692@gmail.com>
2025-07-28 16:36:52 +0200

    update invoice payment detail seeder file ->  delete raw id

--- packages/backend/src/seeders/20250616062547-seed-payment-methods.js
+++ packages/backend/src/seeders/20250616062547-seed-payment-methods.js
@@ -7,37 +7,30 @@ export default {
       },
       [
         {
-          id: 1,
           name: "Punto de venta",
           currency: "Bolivar Digital"
         },
         {
-          id: 2,
           name: "Pago Movil",
           currency: "Bolivar Digital"
         },
-        {
-          id: 3, 
+        { 
           name: "Transferencia",
           currency: "Bolivar Digital"
         },
         {
-          id: 4,
           name: "Efectivo Bolivares",
           currency: "Bolivares"
         },
-        {
-          id: 5, 
+        { 
           name: "Efectivo Dolares",
           currency: "Dolares"
         },
         {
-          id: 6,
           name: "Transferencia Dolares",
           currency: "Dolares"
         },
         {
-          id: 7,
           name: "Cripto",
           currency: "Criptomonedas"
         }
