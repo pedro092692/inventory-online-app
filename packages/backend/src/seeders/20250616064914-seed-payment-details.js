452085480f1418c00bd6f15c34b00c8ee7437e1e
pedro <pedro092692@gmail.com>
2025-07-28 16:37:32 +0200

    update invoice payment detail seeder file ->  delete raw id

--- packages/backend/src/seeders/20250616064914-seed-payment-details.js
+++ packages/backend/src/seeders/20250616064914-seed-payment-details.js
@@ -7,21 +7,18 @@ export default {
       },
       [
         {
-          id: 1,
           invoice_id: 1, 
           payment_id: 1,
           amount: 711.97,
           reference_amount: 6.39
         },
         {
-          id: 2,
           invoice_id: 2, 
           payment_id: 2,
           amount: 631.75,
           reference_amount: 5.67
         },
-        {
-          id: 3, 
+        { 
           invoice_id: 3,
           payment_id: 5,
           amount: 6.06,
