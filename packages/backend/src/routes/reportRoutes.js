a3356dfe8b84194fb8e86efe94d9a0606a2b7a74
pedro <pedro092692@gmail.com>
2025-07-29 17:52:00 +0200

    fix getTopSpendingCustomer function in report service file

--- packages/backend/src/routes/reportRoutes.js
+++ packages/backend/src/routes/reportRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from "express"
-
+import ReportController from "../Controllers/reportController.js";
+import { Invoice } from "../models/inventory_models/InvoiceModel.js";
 
 class ReportRoutes {
     constructor() {
@@ -12,7 +13,8 @@ class ReportRoutes {
      * @returns {void}
      */
     initializeRoutes() {
-        this.router.get('/', (req, res) => res.send("Report routes"))
+        this.router.get("/", (req, res) => res.send("Report routes"))
+        this.router.get("/top-spending-customers", (req, res) => new ReportController(Invoice).getTopSpendingCustomer(req, res))
     }
 }
 
--- packages/backend/src/services/reportService.js
+++ packages/backend/src/services/reportService.js
@@ -1,5 +1,5 @@
-import ServiceErrorHandler from "../errors/ServiceErrorHandler";
-
+import ServiceErrorHandler from "../errors/ServiceErrorHandler.js";
+import { Sequelize } from "sequelize"
 
 
 class ReportService {
@@ -14,10 +14,37 @@ class ReportService {
     getTopSpendingCustomer() {
         return this.#error.handler(["Get best Customers"], async() => {
             const customer = await this.invoice.findAll({
-                order: ['total', 'DESC'],
+                where: {
+                    status: 'paid'
+                },
+                attributes: [
+                    [Sequelize.fn('SUM', Sequelize.col('total')), 'total_spent'],
+                    [Sequelize.fn('MIN', Sequelize.col('date')), 'first_invoice'],
+                    [Sequelize.fn('MAX', Sequelize.col('date')), 'last_invoice']
+                ],
+                include:[
+                    {
+                        association: "customer", attributes: ["name", "phone"]
+                    }
+                ],
+                group: ['customer_id', 'customer.id'],
+                order:[
+                    [[Sequelize.literal('total_spent'), 'DESC']]
+                ],
                 limit: 10
             })
-            return customer
+
+            const formatted_client_list = customer.map((client) => {
+                return {
+                    name: client.customer.name,
+                    total_spent: client.dataValues.total_spent,
+                    first_purchase: client.dataValues.first_invoice.toLocaleDateString('es-VE'),
+                    last_purcahse: client.dataValues.last_invoice.toLocaleDateString('es-VE'),
+                    phone: client.customer.phone
+                    
+                }
+            })
+            return formatted_client_list
         })
     }
 }
