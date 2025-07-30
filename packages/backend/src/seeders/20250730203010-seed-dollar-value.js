855cddd3281d8cceed1b250ca0fcf959b5986e63
pedro <pedro092692@gmail.com>
2025-07-30 22:36:11 +0200

    create dollar-value seeder

---
+++ packages/backend/src/seeders/20250730203010-seed-dollar-value.js
@@ -0,0 +1,28 @@
+import { dollarValues } from "../utils/fakerDollar.js";
+
+export default {
+  up: async (queryInterface, Sequelize, schema) => {
+    await queryInterface.bulkInsert(
+      {
+        tableName: "dollar-value",
+        schema: schema
+      },
+      dollarValues
+    )
+  },
+
+  down: async (queryInterface, Sequelize, schema) => {
+    await queryInterface.bulkDelete(
+      {
+        tableName: "dollar-value",
+        schema: schema
+      },
+      null, // this means "no condition" = delete all rows
+      {
+        truncate: true,
+        restartIdentity: true,
+        cascade: true
+      }
+    )
+  }
+};
