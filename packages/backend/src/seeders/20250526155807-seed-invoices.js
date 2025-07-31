7169fd5856347cd2015b072885683e55a79c1083
pedro <pedro092692@gmail.com>
2025-07-31 14:32:49 +0200

    crate fakerInvoice file

--- packages/backend/src/seeders/20250526155807-seed-invoices.js
+++ packages/backend/src/seeders/20250526155807-seed-invoices.js
@@ -1,3 +1,5 @@
+import { invoicesForDb } from "../utils/fakerInvoice.js";
+
 export default {
   up: async (queryInterface, Sequelize, schema) => {
     await queryInterface.bulkInsert(
@@ -5,32 +7,7 @@ export default {
         tableName: "invoices",
         schema: schema
       },
-      [
-        {
-          date: new Date("2023-05-01T10:00:00Z"),
-          customer_id: 1, // Assuming customer with ID 1 exists
-          seller_id: 1, // Assuming seller with ID 1 exists
-          total: 6.39,
-          total_reference: 6.39 * 112.82,
-          status: "unpaid"
-        },
-        {
-          date: new Date("2023-05-02T11:30:00Z"),
-          customer_id: 2, // Assuming customer with ID 2 exists
-          seller_id: 2, // Assuming seller with ID 2 exists
-          total: 5.67,
-          total_reference: 5.67 * 112.82,
-          status: "unpaid"
-        },
-        {
-          date: new Date("2023-05-03T14:45:00Z"),
-          customer_id: 3, // Assuming customer with ID 3 exists
-          seller_id: 3, // Assuming seller with ID 3 exists
-          total: 6.06,
-          total_reference: 6.06 * 112.82,
-          status: "unpaid"
-        }
-      ],
+      invoicesForDb
     )
 
   },
--- packages/backend/src/utils/fakerInvoice.js
+++ packages/backend/src/utils/fakerInvoice.js
@@ -1,4 +1,5 @@
 import { products } from "./fakerProducts.js"
+import { dollarValues } from "./fakerDollar.js";
 
 class FakerInvoice {
     constructor() {
@@ -6,6 +7,7 @@ class FakerInvoice {
         this.sellerId = Math.floor(Math.random() * 4) + 1;
         this.products = products
         this.productsForInvoice = []
+        this.dollarValues = dollarValues
     }
 
     getProducts() {
@@ -23,10 +25,142 @@ class FakerInvoice {
                 }
             )
         }
-        console.log(this.productsForInvoice)
     }
+
+    generateDates(count = 6) {
+        const today = new Date()
+        const startDate = new Date(today)
+        const dates = []
+        // set date to six months ago 
+        startDate.setMonth(today.getMonth() - count)
+        // set to first day of the month
+        startDate.setDate(1)
+
+        // iterate over dates
+        for(let currentDate = new Date(startDate); currentDate<= today; currentDate.setDate(currentDate.getDate() + 1)) {
+           const dateToAdd = new Date(currentDate)
+           dates.push(dateToAdd)
+        }
+
+        return dates
+    }
+
+    generateInvoices(dates) {
+        const invoices = []
+        let i = 0
+        let j = 1
+        // iterate over dates
+        for(let date of dates) {
+            
+            const randomUser = this._randomId(150)
+            const randomSeller = this._randomId(3)
+            const numberOfProduct = this._randomNumberProducts()
+            const products = this._getRandomProducts(numberOfProduct, 499)
+            const total = this._getInvoiceTotal(products)
+            const dollarValue = this.dollarValues[i].value
+            invoices.push(
+                {   
+                    invoice_id: j,
+                    date: date,
+                    customer_id: randomUser,
+                    seller_id: randomSeller,
+                    // numberOfProduct: numberOfProduct,
+                    products: products,
+                    total: total,
+                    total_reference: total * dollarValue,
+                    total_paid: total,
+                    status: "paid"
+                }
+            )
+            
+            i++
+            j++
+            if([5,6].includes(date.getDay()) ){
+               i--
+            }
+            
+            
+        }
+        return invoices
+    }
+
+    invoiceForDb(invoices) {
+        return invoices.map((invoice) => {
+            return {
+                date: invoice.date,
+                customer_id: invoice.customer_id,
+                seller_id: invoice.seller_id,
+                total: invoice.total,
+                total_reference: invoice.total_reference,
+                total_paid: invoice.total_paid,
+                status: invoice.status
+            }
+        })
+    }
+
+    _randomId(max = 150) {
+        return Math.floor(Math.random() * max) + 1
+    }
+
+    _getRandomProducts(numberOfProducts, productCount = 499) {
+        const products = {}
+        const invoiceProducts = []
+        
+        //get random products from products array 
+        for(let item = 0; item<numberOfProducts; item++) {
+            //get random product id 
+            const randomProductId = this._randomId(productCount)
+            if(!(randomProductId in products)){
+                const quantity = this._randomId(3)
+                products[randomProductId] = {
+                    id: randomProductId,
+                    name: this.products[randomProductId].name,
+                    price: this.products[randomProductId].selling_price,
+                    quantity: quantity,
+                    total: this.products[randomProductId].selling_price * quantity
+                }
+            }         
+        }
+
+        for(let product in products) {
+            invoiceProducts.push(
+                {
+                    product_id: products[product].id,
+                    name: products[product].name,
+                    price: products[product].price,
+                    quantity: products[product].quantity,
+                    total: products[product].total
+                }
+            )
+        }
+        
+        return invoiceProducts
+    }
+
+    _randomNumberProducts() {
+        // generate random number of products, with range between 1 and 7 products
+        let numberProducts = Math.floor(Math.random() * 7) + 1
+        
+        if(numberProducts == 5 && Math.floor(Math.random() * 9) < 7){
+            numberProducts = 1
+        }
+
+        return numberProducts
+
+    }
+
+    _getInvoiceTotal(products) {
+        return products.reduce((sum, currentProduct) => {
+            return sum + currentProduct.total
+        }, 0) 
+    }
+
 
 }
 
 const faker = new FakerInvoice()
-console.log(faker.getProducts())
+const dates = faker.generateDates()
+const invoices = faker.generateInvoices(dates)
+const invoicesForDb = faker.invoiceForDb(invoices)
+
+export {invoices, invoicesForDb}
