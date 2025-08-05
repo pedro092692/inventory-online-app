b1472f1e21c8860099724bc5f6436555c25d68c4
pedro <pedro092692@gmail.com>
2025-08-05 12:50:53 +0200

    docs: add JDocs comments to invoiceDetailModel file

--- packages/backend/src/models/inventory_models/InvoiceDetailModel.js
+++ packages/backend/src/models/inventory_models/InvoiceDetailModel.js
@@ -4,7 +4,11 @@ import { DataTypes, Model } from 'sequelize'
 class InvoiceDetail extends Model {
     // model ralations
 
-    // invoices: 
+    /**
+     * Creates an association between the InvoiceDetail model and the Invoice model.
+     * @param {{ Invoice: typeof Model }} model - An object containing the Invoice model class.
+     * @returns {void} This method does not return a value.
+     */
     static associationInvoice(model) {
         this.belongsTo(model.Invoice, {
             foreignKey: 'invoice_id',
@@ -12,7 +16,11 @@ class InvoiceDetail extends Model {
         })
     }
 
-    // products 
+    /**
+     * Creates an association between the InvoiceDetail model and the Product model.
+     * @param {{ Product: typeof Model}} model - An object containing the Product model class.
+     * @returns {void} This method does not return a value. 
+     */
     static associationProducts(model) {
         this.belongsTo(model.Product, {
             foreignKey: 'product_id',
@@ -21,6 +29,13 @@ class InvoiceDetail extends Model {
     }
 }
 
+/**
+ * Initializes the Invoice model with its schema definition and configuration.
+ * This function sets up the Invoice model with fields such as `id`, `invoice_id`, `product_id`, `quantity` and `unit_price`,
+ * and configures Sequelize options like table name, schema, and timestamps.
+ * @param {import('sequelize').Sequelize} sequelize - The Sequelize instance used to initialize the model.
+ * @returns {void} This function does not return a value.
+ */
 function initializeInvoiceDetail(sequelize) {
     InvoiceDetail.init(
         {
