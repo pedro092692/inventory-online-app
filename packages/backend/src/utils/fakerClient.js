4d65a3abbeb460a1c4625559701d695c677229c3
pedro <pedro092692@gmail.com>
2025-08-05 18:06:37 +0200

    docs: Add JSDoc comments to fakerClient file

--- packages/backend/src/utils/fakerClient.js
+++ packages/backend/src/utils/fakerClient.js
@@ -1,5 +1,10 @@
 import { faker } from '@faker-js/faker';
 
+/**
+ * Generates a list of fake customers with the following fields: id_number, name, and phone.
+ * @param {Number} count Number of fake customers to generate.
+ * @returns {Array<{ id_number: number, name: string, phone: string }>} Array of fake customer objects.
+ */
 function fakerCustomer(count = 150) {
     const customers = Array.from({ length: count }, () => ({
         id_number: Math.floor(Math.random() * (30000000 - 3000000 + 1)) + 3000000,
