bc077b005bf9a6804f97c70ddf5411f1c3432655
pedro <pedro092692@gmail.com>
2025-08-06 11:27:57 +0200

    feat: add findUser method in SecurityController file

---
+++ packages/backend/src/Controllers/admin/SecurityController.js
@@ -0,0 +1,33 @@
+import UserService from '../../services/admin/UserService.js'
+import ControllerErrorHandler from '../../errors/controllerErrorHandler.js'
+
+
+class SecurityController {
+    // new instance of controller error handler 
+    #error = new ControllerErrorHandler()
+
+    constructor() {
+        this.user = new UserService()
+        this.#error
+    }
+
+    /**
+     * Retrieves a user by a given email.
+     * @param {Object} req - request object.
+     * @param {Object} res - response object to send the user info.
+     * @throws {ServiceError} - throws an error if the users could not be retrieved.
+     * @returns {Promise<void>} - returns the user data in the response.
+     */
+    findUser = this.#error.handler( async(req, res) => {
+        const { email } = req.body
+        const user = await this.user.findUserByEmail(email)
+        
+        if(!user) {
+            res.status(401).json({message: 'Involid Email or Password'})
+        }
+
+        res.status(200).json({message: 'User found!`'})
+    })
+}
+
+export default SecurityController
