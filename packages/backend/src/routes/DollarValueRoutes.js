95a361064ec50e6685bec11903a2b4202eadce09
pedro <pedro092692@gmail.com>
2025-08-07 12:51:05 +0200

    feat: add authMiddleware to dollarValue routes and set the models for the route

--- packages/backend/src/routes/DollarValueRoutes.js
+++ packages/backend/src/routes/DollarValueRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
-import { Dollar } from '../models/inventory_models/DollarModel.js'
 import DollarValueController from '../Controllers/DollarController.js'
+import { authenticated } from '../middlewares/authMiddleware.js'
 
 class DollarValueRoutes{
     constructor() {
         this.router = Router()
+        this.router.use(authenticated)
+        this.router.use(this.setRoutesModels.bind(this))
         this.initializeRoutes()
     }
 
@@ -14,10 +16,19 @@ class DollarValueRoutes{
      */
     initializeRoutes() {    
         this.router.get('/', (req, res) => res.send('Dollar Value Routes'))
-        this.router.post('/', (req, res) => new DollarValueController(Dollar).createDollarValue(req, res))
-        this.router.get('/latest', (req, res) => new DollarValueController(Dollar).getLastValue(req, res))
-        this.router.patch('/', (req, res) => new DollarValueController(Dollar).updateDollarValue(req, res))
-        this.router.delete('/', (req, res) => new DollarValueController(Dollar).deleteDollarValue(req, res))
+        this.router.post('/', (req, res) => new DollarValueController(req.Dollar).createDollarValue(req, res))
+        this.router.get('/latest', (req, res) => new DollarValueController(req.Dollar).getLastValue(req, res))
+        this.router.patch('/', (req, res) => new DollarValueController(req.Dollar).updateDollarValue(req, res))
+        this.router.delete('/', (req, res) => new DollarValueController(req.Dollar).deleteDollarValue(req, res))
+    }
+
+    async setRoutesModels(req, res, next) {
+        const {Dollar} = req.tenantModels
+        if(!Dollar) {
+            return res.status(400).json({ message: 'Customer model is required' })
+        }
+        req.Dollar = Dollar
+        next()
     }
 }
 
