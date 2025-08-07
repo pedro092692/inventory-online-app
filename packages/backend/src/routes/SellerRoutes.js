a8c1567dc69613ca6b85ff0ddf9ef5c2db07ef87
pedro <pedro092692@gmail.com>
2025-08-07 13:24:20 +0200

    feat: add authMiddleware to Seller routes and set the models for the route

--- packages/backend/src/routes/SellerRoutes.js
+++ packages/backend/src/routes/SellerRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import SellerController from '../Controllers/SellerController.js'
-import { Seller } from '../models/inventory_models/SellerModel.js'
+import { authenticated } from '../middlewares/authMiddleware.js'
 
 class SellerRoutes{
     constructor() {
         this.router = Router()
+        this.router.use(authenticated)
+        this.router.use(this.setRoutesModels.bind(this))
         this.inicializateRoutes()
     }  
     /**
@@ -13,11 +15,20 @@ class SellerRoutes{
      */
     inicializateRoutes() {
         this.router.get('/', (req, res) => res.send('Seller routes'))
-        this.router.get('/all', (req, res) => new SellerController(Seller).allSeller(req, res))
-        this.router.get('/:id', (req, res) => new SellerController(Seller).getSeller(req, res))
-        this.router.post('/', (req, res) => new SellerController(Seller).createSeller(req, res))
-        this.router.patch('/:id', (req, res) => new SellerController(Seller).updateSeller(req, res))
-        this.router.delete('/', (req, res) => new SellerController(Seller).deleteSeller(req, res))
+        this.router.get('/all', (req, res) => new SellerController(req.Seller).allSeller(req, res))
+        this.router.get('/:id', (req, res) => new SellerController(req.Seller).getSeller(req, res))
+        this.router.post('/', (req, res) => new SellerController(req.Seller).createSeller(req, res))
+        this.router.patch('/:id', (req, res) => new SellerController(req.Seller).updateSeller(req, res))
+        this.router.delete('/', (req, res) => new SellerController(req.Seller).deleteSeller(req, res))
+    }
+
+    async setRoutesModels(req, res, next) {
+        const {Seller} = req.tenantModels
+        if(!Seller) {
+            return res.status(400).json({ message: 'Seller model is required' })
+        }
+        req.Seller = Seller
+        next()
     }
 }
 
