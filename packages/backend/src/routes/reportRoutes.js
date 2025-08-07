73fcdc7211e43a8178d4927e3349e7a0756113e4
pedro <pedro092692@gmail.com>
2025-08-07 13:21:18 +0200

    feat: add authMiddleware to Report routes and set the models for the route

--- packages/backend/src/routes/reportRoutes.js
+++ packages/backend/src/routes/reportRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from 'express'
 import ReportController from '../Controllers/reportController.js'
-import { Invoice } from '../models/inventory_models/InvoiceModel.js'
-import { InvoiceDetail } from '../models/inventory_models/InvoiceDetailModel.js'
-import { PaymentDetail } from '../models/inventory_models/PaymentDetailModel.js'
+import { authenticated } from '../middlewares/authMiddleware.js'
 
 class ReportRoutes {
     constructor() {
         this.router = Router()
+        this.router.use(authenticated)
+        this.router.use(this.setRoutesModels.bind(this))
         this.initializeRoutes()
     }
 
@@ -16,19 +16,30 @@ class ReportRoutes {
      */
     initializeRoutes() {
         this.router.get('/', (req, res) => res.send('Report routes'))
-        this.router.get('/top-spending-customers', (req, res) => new ReportController(Invoice).getTopSpendingCustomer(req, res))
-        this.router.get('/top-recurring-customers', (req, res) => new ReportController(Invoice).getTopRecurringCustomer(req, res))
-        this.router.get('/top-selling-products', (req, res) => new ReportController(null, InvoiceDetail ).getTopSellingProducts(req, res))
-        this.router.get('/worst-selling-products', (req, res) => new ReportController(null, InvoiceDetail ).getWorstWellingProducts(req, res))
-        this.router.get('/best-selling-day', (req, res) => new ReportController(Invoice).bestSellingDay(req, res))
-        this.router.get('/worst-selling-day', (req, res) => new ReportController(Invoice).worstSellingDay(req, res))
-        this.router.get('/sales-per-day', (req, res) => new ReportController(Invoice).salesPerDay(req, res))
-        this.router.get('/peak-sales-hour', (req, res) => new ReportController(Invoice).peakSalesHour(req, res))
-        this.router.get('/peak-day-week', (req, res) => new ReportController(Invoice).peakSalesDayOfWeek(req, res))
-        this.router.get('/detail-sales', (req, res) => new ReportController(null, null, PaymentDetail).salesDetail(req, res))
-        this.router.get('/invoices-per-day', (req, res) => new ReportController(Invoice).invoicePerDate(req, res))
-        this.router.get('/cash-closing', (req, res) =>  new ReportController(null, null, PaymentDetail).cashClosing(req, res))
-        this.router.get('/pay-methods', (req, res) =>  new ReportController(null, null, PaymentDetail).payMethodPercent(req, res))
+        this.router.get('/top-spending-customers', (req, res) => new ReportController(req.Invoice).getTopSpendingCustomer(req, res))
+        this.router.get('/top-recurring-customers', (req, res) => new ReportController(req.Invoice).getTopRecurringCustomer(req, res))
+        this.router.get('/top-selling-products', (req, res) => new ReportController(null, req.InvoiceDetail ).getTopSellingProducts(req, res))
+        this.router.get('/worst-selling-products', (req, res) => new ReportController(null, req.InvoiceDetail ).getWorstWellingProducts(req, res))
+        this.router.get('/best-selling-day', (req, res) => new ReportController(req.Invoice).bestSellingDay(req, res))
+        this.router.get('/worst-selling-day', (req, res) => new ReportController(req.Invoice).worstSellingDay(req, res))
+        this.router.get('/sales-per-day', (req, res) => new ReportController(req.Invoice).salesPerDay(req, res))
+        this.router.get('/peak-sales-hour', (req, res) => new ReportController(req.Invoice).peakSalesHour(req, res))
+        this.router.get('/peak-day-week', (req, res) => new ReportController(req.Invoice).peakSalesDayOfWeek(req, res))
+        this.router.get('/detail-sales', (req, res) => new ReportController(null, null, req.PaymentDetail).salesDetail(req, res))
+        this.router.get('/invoices-per-day', (req, res) => new ReportController(req.Invoice).invoicePerDate(req, res))
+        this.router.get('/cash-closing', (req, res) =>  new ReportController(null, null, req.PaymentDetail).cashClosing(req, res))
+        this.router.get('/pay-methods', (req, res) =>  new ReportController(null, null, req.PaymentDetail).payMethodPercent(req, res))
+    }
+
+    async setRoutesModels(req, res, next) {
+        const {Invoice, InvoiceDetail, PaymentDetail} = req.tenantModels
+        if(!Invoice || !InvoiceDetail || !PaymentDetail) {
+            return res.status(400).json({ message: 'Invoice, InvoiceDetail and PaymentDetail models are required' })
+        }
+        req.Invoice = Invoice
+        req.InvoiceDetail = InvoiceDetail
+        req.PaymentDetail = PaymentDetail
+        next()
     }
 }
 
