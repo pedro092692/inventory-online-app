ed22a2bb149c2eef800a0f2896673ae6912e6dd7
pedro <pedro092692@gmail.com>
2025-08-07 13:16:44 +0200

    feat: add authMiddleware to Product routes and set the models for the route

--- packages/backend/src/routes/ProductRoutes.js
+++ packages/backend/src/routes/ProductRoutes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import ProductController from '../Controllers/ProductController.js'
-import {Product} from '../models/inventory_models/ProductModel.js'
-import { Dollar } from '../models/inventory_models/DollarModel.js'
+import { authenticated } from '../middlewares/authMiddleware.js'
 
 class ProductRoutes {
     constructor(){
         this.router = Router()
+        this.router.use(authenticated)
+        this.router.use(this.setRoutesModels.bind(this))
         this.initializeRoutes()
     }
     /**
@@ -14,11 +15,21 @@ class ProductRoutes {
      */
     initializeRoutes() {
         this.router.get('/', (req, res) => res.send('Product routes'))
-        this.router.get('/all', (req, res) => new ProductController(Product).allProducts(req, res))
-        this.router.get('/:id', (req, res) => new ProductController(Product, Dollar).getProduct(req, res))
-        this.router.post('/', (req, res) => new ProductController(Product).createProduct(req, res))
-        this.router.patch('/:id', (req, res) => new ProductController(Product).updateProduct(req, res))
-        this.router.delete('/', (req, res) => new ProductController(Product).deleteProduct(req, res))
+        this.router.get('/all', (req, res) => new ProductController(req.Product).allProducts(req, res))
+        this.router.get('/:id', (req, res) => new ProductController(req.Product, req.Dollar).getProduct(req, res))
+        this.router.post('/', (req, res) => new ProductController(req.Product).createProduct(req, res))
+        this.router.patch('/:id', (req, res) => new ProductController(req.Product).updateProduct(req, res))
+        this.router.delete('/', (req, res) => new ProductController(req.Product).deleteProduct(req, res))
+    }
+
+    async setRoutesModels(req, res, next) {
+        const {Product, Dollar} = req.tenantModels
+        if(!Dollar || !Product) {
+            return res.status(400).json({ message: 'Dollar and Product models are required' })
+        }
+        req.Product = Product
+        req.Dollar = Dollar
+        next()
     }
 }
 
