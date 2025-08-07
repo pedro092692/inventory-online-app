5778fd658115e04cb8620daca7381707d0e1d89e
pedro <pedro092692@gmail.com>
2025-08-07 13:10:45 +0200

    feat: add authMiddleware to PaymentMethods routes and set the models for the route

--- packages/backend/src/routes/PaymentMethodRoutes.js
+++ packages/backend/src/routes/PaymentMethodRoutes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import PaymentMethodController from '../Controllers/PaymentMethodController.js'
-import { Payment } from '../models/inventory_models/PaymentModel.js'
+import { authenticated } from '../middlewares/authMiddleware.js'
 
 class PaymentMethodRoutes { 
     
     constructor() {
         this.router = Router()
+        this.router.use(authenticated)
+        this.router.use(this.setRoutesModel.bind(this))
         this.initializeRoutes()
     }
 
@@ -15,11 +17,20 @@ class PaymentMethodRoutes {
      */
     initializeRoutes() {
         this.router.get('/', (req, res) => res.send('Payment Methods'))
-        this.router.get('/all', (req, res) => new PaymentMethodController(Payment).getAllProducts(req, res))
-        this.router.get('/:id', (req, res) => new PaymentMethodController(Payment).getPaymentMethod(req, res))
-        this.router.post('/', (req, res) => new PaymentMethodController(Payment).createPaymentMethod(req, res))
-        this.router.patch('/:id', (req, res) => new PaymentMethodController(Payment).updatePaymentMethod(req, res))
-        this.router.delete('/', (req, res) => new PaymentMethodController(Payment).deletePaymentMethod(req, res))
+        this.router.get('/all', (req, res) => new PaymentMethodController(req.Payment).getAllProducts(req, res))
+        this.router.get('/:id', (req, res) => new PaymentMethodController(req.Payment).getPaymentMethod(req, res))
+        this.router.post('/', (req, res) => new PaymentMethodController(req.Payment).createPaymentMethod(req, res))
+        this.router.patch('/:id', (req, res) => new PaymentMethodController(req.Payment).updatePaymentMethod(req, res))
+        this.router.delete('/', (req, res) => new PaymentMethodController(req.Payment).deletePaymentMethod(req, res))
+    }
+
+    async setRoutesModel(req, res, next) {
+        const { Payment } = req.tenantModels
+        if(!Payment) {
+            return res.status(400).json({ message: 'Payment model is required' })
+        }
+        req.Payment = Payment
+        next()
     }
 }
 
