ddff86c23aedca022ee4fe800e180d4141cec4ad
pedro <pedro092692@gmail.com>
2025-08-07 13:06:20 +0200

    feat: add authMiddleware to PaymentDetail routes and set the models for the route

--- packages/backend/src/routes/PaymentDetailRoutes.js
+++ packages/backend/src/routes/PaymentDetailRoutes.js
@@ -1,13 +1,13 @@
 import { Router } from 'express'
-import { PaymentDetail } from '../models/inventory_models/PaymentDetailModel.js'
-import { Dollar } from '../models/inventory_models/DollarModel.js'
-import { Invoice } from '../models/inventory_models/InvoiceModel.js'
 import PayInvoiceController from '../Controllers/PaymentInvoiceDetailController.js'
+import { authenticated } from '../middlewares/authMiddleware.js'
 
 class PayInvoiceRoutes {
 
     constructor() {
         this.router = Router()
+        this.router.use(authenticated)
+        this.router.use(this.setRoutesModels.bind(this))
         this.initializeRoutes()
     }
 
@@ -17,10 +17,21 @@ class PayInvoiceRoutes {
      */
     initializeRoutes() {
         this.router.get('/', (req, res) => res.send('Pay Invoice Detail Route'))
-        this.router.get('/:id', (req, res) => new PayInvoiceController(PaymentDetail).getPaymentDetail(req, res))
-        this.router.post('/', (req, res) => new PayInvoiceController(PaymentDetail, Dollar, Invoice).createPaymentInvoiceDetail(req, res))
-        this.router.patch('/:id', (req, res) => new PayInvoiceController(PaymentDetail, Dollar, Invoice).updatePaymentDetail(req, res))
-        this.router.delete('/', (req, res) => new PayInvoiceController(PaymentDetail, Dollar, Invoice).deletePaymentDetail(req, res))
+        this.router.get('/:id', (req, res) => new PayInvoiceController(req.PaymentDetail).getPaymentDetail(req, res))
+        this.router.post('/', (req, res) => new PayInvoiceController(req.PaymentDetail, req.Dollar, req.Invoice).createPaymentInvoiceDetail(req, res))
+        this.router.patch('/:id', (req, res) => new PayInvoiceController(req.PaymentDetail, req.Dollar, req.Invoice).updatePaymentDetail(req, res))
+        this.router.delete('/', (req, res) => new PayInvoiceController(req.PaymentDetail, req.Dollar, req.Invoice).deletePaymentDetail(req, res))
+    }
+
+    async setRoutesModels(req, res, next) {
+        const {PaymentDetail, Dollar, Invoice} = req.tenantModels
+        if(!Dollar || !PaymentDetail || !Invoice) {
+            return res.status(400).json({ message: 'Dollar, Invoice and PaymentDetail model are required' })
+        }
+        req.PaymentDetail = PaymentDetail
+        req.Invoice = Invoice
+        req.Dollar = Dollar
+        next()
     }
 }
 
