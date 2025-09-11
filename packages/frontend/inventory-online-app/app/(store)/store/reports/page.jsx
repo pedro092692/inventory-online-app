ba58f0fa98c8c29f6f08ef9fab190034a8026a42
pedro <pedro092692@gmail.com>
2025-09-11 17:36:23 +0200

    feat: create reports route

---
+++ packages/frontend/inventory-online-app/app/(store)/store/reports/page.jsx
@@ -0,0 +1,9 @@
+import { Title } from "@/app/ui/dashboard/title/title"
+
+export default function Customer() {
+    return (
+        <>
+            <Title title="Reportes" icon='report'/>
+        </>
+    )
+}
--- packages/frontend/inventory-online-app/app/ui/dashboard/panel/panel.jsx
+++ packages/frontend/inventory-online-app/app/ui/dashboard/panel/panel.jsx
@@ -48,7 +48,7 @@ export function Panel() {
         reports: {
             title: 'Reportes',
             icon: 'report',
-            link: '#'
+            link: '/store/reports'
         },
 
         sell: {
