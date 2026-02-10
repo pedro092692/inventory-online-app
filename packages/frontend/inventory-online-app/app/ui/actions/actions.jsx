47eb235e2a1016342e0bdcfbdb8099263dfb3def
pedro <pedro092692@gmail.com>
2026-02-10 17:18:16 +0100

    feat: add show and hide option in actions component

--- packages/frontend/inventory-online-app/app/ui/actions/actions.jsx
+++ packages/frontend/inventory-online-app/app/ui/actions/actions.jsx
@@ -5,57 +5,64 @@ import { Icon } from '@/app/ui/utils/icons/icons'
 import { Button } from '@/app/ui/utils/button/buttons'
 import Link from 'next/link'
 
-export default function Actions ({currentUser={permissions:[]}, urlPath='customers', id=1, params=''}) {
+export default function Actions ({currentUser={permissions:[]}, urlPath='customers', id=1, params='', showView=true, showEdit=true, showDelete=true}) {
     const path = '/store'
     const view = (href=`${path}/${urlPath}/view/detail/${id}${params?params:''}`) => {
-        return (
-            <Link href={href}>
-                <Button 
-                    children={false}
-                    showIcon={true}
-                    icon={'view'}
-                    type={'secondary'}
-                    size={[15, 15]}
-                    style={{padding: '3px 5px'}}
-                    title={'Ver'}
-                />
-            </Link>
-        )
+        if (showView){
+            return (
+                <Link href={href}>
+                    <Button 
+                        children={false}
+                        showIcon={true}
+                        icon={'view'}
+                        type={'secondary'}
+                        size={[15, 15]}
+                        style={{padding: '3px 5px'}}
+                        title={'Ver'}
+                    />
+                </Link>
+            )
+        }
     }
 
     const edit = (href=`${path}/${urlPath}/edit/${id}${params?params:''}`) => {
-        return (
-            <Link href={href}>
-                <Button 
-                    children={false}
-                    showIcon={true}
-                    icon={'edit'}
-                    type={'warning'}
-                    size={[15, 15]}
-                    style={{padding: '3px 5px'}}
-                    title={'Editar'}
-                />
-            </Link>
-        )
+        if (showEdit){
+            return (
+                <Link href={href}>
+                    <Button 
+                        children={false}
+                        showIcon={true}
+                        icon={'edit'}
+                        type={'warning'}
+                        size={[15, 15]}
+                        style={{padding: '3px 5px'}}
+                        title={'Editar'}
+                    />
+                </Link>
+            )
+        }
     }
 
     const remove = (href=`${path}/${urlPath}/delete/${id}`) => {
-        return (
-            <Link href={href}>
-                <Button 
-                    children={false}
-                    showIcon={true}
-                    icon={'trash'}
-                    type={'danger'}
-                    size={[15, 15]}
-                    style={{padding: '3px 5px'}}
-                    title={'Eliminar'}
-                />
-            </Link>
-        )
+        if (showDelete){
+            return (
+                <Link href={href}>
+                    <Button 
+                        children={false}
+                        showIcon={true}
+                        icon={'trash'}
+                        type={'danger'}
+                        size={[15, 15]}
+                        style={{padding: '3px 5px'}}
+                        title={'Eliminar'}
+                    />
+                </Link>
+            )
+        }
     }
 
     if (currentUser.permissions.includes('delete')) {
+        
         return (
             <Container 
                 padding={'0px'}
@@ -65,8 +72,8 @@ export default function Actions ({currentUser={permissions:[]}, urlPath='custome
                 {edit()}
                 {remove()}
             </Container>
-        )
-    
+            )
+
     }
 
     if (currentUser.permissions.includes('edit')) {
