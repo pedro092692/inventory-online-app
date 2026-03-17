3b628d4f0720fd66426dd5883d1eab4c41168c18
pedro <pedro092692@gmail.com>
2026-03-17 17:07:17 +0100

    refator: fetch function in utils

--- packages/frontend/inventory-online-app/app/utils/fetch.js
+++ packages/frontend/inventory-online-app/app/utils/fetch.js
@@ -3,27 +3,31 @@ import { cookies } from 'next/headers'
 export default async function FetchDataTest(url, method, body = null) {
     const cookieStore = await cookies()
     const token = cookieStore.get('access_token')?.value
-    await new Promise (resolve => setTimeout(resolve, 1000))
+    // await new Promise (resolve => setTimeout(resolve, 100))
     if (!token) {
         return null
     }
 
+    
     const response = await fetch(url, {
-        method,
+        method: method,
         headers: {
             'Cookie': `access_token=${token}`,
             'Content-Type': 'application/json'
         },
         body: body ? JSON.stringify(body) : null
-        
     })
-  
+    
+    const data = await response.json()
 
     if (!response.ok) {
-        throw { status: response.status, message: 'Somthing went wrong' }
+        if (response.status === 500){
+            throw new Error('Something went wrong')
+        }
+        throw new Error(data?.error || `Error: ${response.status}`)
     }
 
-    return response.json()
-    
+    return data
+
 }
     
