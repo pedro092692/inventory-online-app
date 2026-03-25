58cbdc11bbfaeb1d99c88f639baee04008ac34b3
pedro092692 <pedro092692@gmail.com>
2026-03-25 14:18:12 +0100

    refactor delete util function handlePageChange

--- packages/frontend/inventory-online-app/app/utils/handlePageChange.js
+++
@@ -1,9 +0,0 @@
-export default async function HandlePageChange(currentPage, setOffset, limit, fetchDataFn, searchTerm=null) {
-    setOffset((currentPage - 1) * limit)
-    if (!searchTerm) {
-        await fetchDataFn(limit, (currentPage - 1) * limit)
-    }else {
-        await fetchDataFn(limit, (currentPage - 1) * limit, searchTerm)
-    
-    }
-}
