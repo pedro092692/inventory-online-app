62850c2e882277cc6c2875444b7eced5b4408923
pedro092692 <pedro092692@gmail.com>
2026-03-26 14:21:20 +0100

    refactor customerInvoices component

--- packages/frontend/inventory-online-app/app/(store)/store/customers/_components/detail/customerInvoices.jsx
+++ packages/frontend/inventory-online-app/app/(store)/store/customers/_components/detail/customerInvoices.jsx
@@ -1,18 +1,14 @@
-import FetchData from '@/app/utils/fetch'
-import { withErrorHandler } from '@/app/errors/withErrorHandler'
 import CustomerInvoices from '@/app/(store)/store/customers/_components/invoices/invoices'
-const NEXT_PUBLIC_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://127.0.0.1'
+import GetItemAction from '@/app/lib/actions/get'
 
 export default async function CustomerInvoicesWrapper({id, page, invoiceQuery, limit = 8}) {
-    
     const endpoint = invoiceQuery
-    ? `/api/invoices/search?invoice=${invoiceQuery}&customer_id=${id}&limit=${limit}&invoice_page=${page}`
-    : `/api/customers/${id}?limitInvoices=${limit}&pageInvoices=${page}` 
+    ? `invoices/search?invoice=${invoiceQuery}&customer_id=${id}&limit=${limit}&invoice_page=${page}`
+    : `customers/${id}?limitInvoices=${limit}&pageInvoices=${page}` 
     
-    const url = `${NEXT_PUBLIC_API_BASE_URL}${endpoint}`
-    const fetch = withErrorHandler(FetchData, 'hubo un error inesperado al cargar las facturas del cliente')
+   
  
-    const response = await fetch(url, 'GET')
+    const response = await GetItemAction(endpoint)
     const {data, error} = response
     let invoices = data?.customer?.invoices || data?.invoices ||[]
     if (error) {
