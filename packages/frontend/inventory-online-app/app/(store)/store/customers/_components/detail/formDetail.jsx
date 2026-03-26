b1ce673a468685a8d26b03cf3b4fc03a5d0de179
pedro092692 <pedro092692@gmail.com>
2026-03-26 14:30:52 +0100

    refactor delete lib\customer\action folfer

--- packages/frontend/inventory-online-app/app/(store)/store/customers/_components/detail/formDetail.jsx
+++ packages/frontend/inventory-online-app/app/(store)/store/customers/_components/detail/formDetail.jsx
@@ -11,7 +11,7 @@ export default function CustomerDetailForm({customer, readOnly = true}){
             <Form className={`${styles.formview} shadow`}>
                 <Input type="text" icon="person" value={`${customer?.name}`} name={'name'} className={styles.inputReadOnly} readOnly={readOnly}/>
                 <Input type="text" icon="id" value={`${customer?.id_number}`} name={'id_number'} className={styles.inputReadOnly} readOnly={readOnly}/>
-                <Input type="text" icon="phone" value={`${customer?.phone}`} name={'cellphone'} className={styles.inputReadOnly} readOnly={readOnly}/>        
+                <Input type="phone" icon="phone" value={`${customer?.phone}`} name={'phone'} className={styles.inputReadOnly} readOnly={readOnly}/>        
             </Form>
         }
         </>
