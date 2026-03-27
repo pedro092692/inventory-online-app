7167dbdf049a7c11f059273d293fbe98fdb599b9
pedro092692 <pedro092692@gmail.com>
2026-03-28 00:15:06 +0100

    refactor: customers detail

--- packages/frontend/inventory-online-app/app/(store)/store/customers/_components/edit/details.jsx
+++ packages/frontend/inventory-online-app/app/(store)/store/customers/_components/edit/details.jsx
@@ -1,15 +1,11 @@
-import FetchData from '@/app/utils/fetch'
-import { withErrorHandler } from '@/app/errors/withErrorHandler'
+import Request from '@/app/utils/request'
 import CustomerDetailForm from '@/app/(store)/store/customers/_components/edit/EditFormDetail'
-
-const NEXT_PUBLIC_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://127.0.0.1'
 
 export default async function CustomerInfo({id}) {
     
-    const endpoint = `/api/customers/${id}`
-    const url = `${NEXT_PUBLIC_API_BASE_URL}${endpoint}?limitInvoices=0`
-    const fetch = withErrorHandler(FetchData, 'Hubo un error inesperado intententa nuevamente')
-    const response = await fetch(url, 'GET')
+    const endpoint = `customers/${id}`
+    const url = `${endpoint}?limitInvoices=0`
+    const response = await  Request(url, 'GET', null, 'Hubo un error inesperado intententa nuevamente')
     const {data, error} = response
  
     const customer = data?.customer || null
