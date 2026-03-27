983ca11d07549ee8c583fcc4304ce4d5e0794f70
pedro092692 <pedro092692@gmail.com>
2026-03-28 00:53:23 +0100

    feat: create add product component

---
+++ packages/frontend/inventory-online-app/app/(store)/store/products/_components/add/addProductForm.jsx
@@ -0,0 +1,99 @@
+'use client'
+import AddItemAction from '@/app/lib/actions/add'
+import { Form } from '@/app/ui/form/form/form'
+import { Input } from '@/app/ui/form/input/input'
+import { Button } from '@/app/ui/utils/button/buttons'
+import { OvalLoader } from '@/app/ui/loader/spinner'
+import { useActionState, useState, useEffect } from 'react'
+
+export default function AddProductForm() {
+    const initialState = {message: null, inputs: {}, errors: {}}
+    const addProduct = AddItemAction.bind(null, 'products', ['name', 'barcode', 'purchase_price', 'selling_price', 'stock'], 
+        'Producto agregado con éxito')
+    const [state, formAction, isPending] = useActionState(addProduct, initialState)
+    const [field, setField] = useState({name: {isEdited: false,}, barcode: {isEdited: false}, 
+                                        purchase_price: {isEdited: false}, selling_price: {isEdited: false}, stock: 
+                                        {isEdited: false}}) 
+    
+    const hasChanges = 
+                field.name.isEdited ||
+                field.barcode.isEdited ||
+                field.purchase_price.isEdited ||
+                field.selling_price.isEdited ||
+                field.stock.isEdited
+    
+                const handleSubmit = (formData) => {
+        if(!hasChanges) return
+        
+        formAction(formData)
+    }
+
+    useEffect(() => {
+        const success = state?.message 
+        if (success) {
+            setField({
+                name: {isEdited: false},
+                barcode: {isEdited: false},
+                purchase_price: {isEdited: false},
+                selling_price: {isEdited: false},
+                stock: {isEdited: false}
+            })
+        }
+    }, [state])
+
+    return (
+        <Form className={'form-edit'} style={{padding: "16px"}} action={handleSubmit}  >
+            <label>Nombre del producto</label>
+            <Input type="text" placeHolder="Nombre del producto" icon="product" 
+            defaultValue={state.inputs?.name ?? ""} name={'name'} 
+            onChange={() => setField({...field, name: {isEdited: true}})}
+            capitalize={true}/>
+
+            {state?.errors?.name && <span className="field_error">{state?.errors?.name}</span>}
+            
+            <label>Codigo de barras</label>
+            <Input type="text" placeHolder="Código de barras" icon="barcode" 
+            defaultValue={state.inputs?.barcode ?? ""}
+            onChange={() => setField({...field, barcode: {isEdited: true}})}
+            name={'barcode'} />
+
+            {state?.errors?.barcode && <span className="field_error">{state?.errors?.barcode}</span>}
+            
+
+            <label>Precio de compra $</label>
+            <Input type="text" placeHolder="Precio de compra $" icon="dollar" 
+            defaultValue={state.inputs?.purchase_price ?? ""}
+            onChange={() => setField({...field, purchase_price: {isEdited: true}})}  
+            name={'purchase_price'} />
+
+            {state?.errors?.purchase_price && <span className="field_error">{state?.errors?.purchase_price}</span>}
+
+            <label>Precio de venta $</label>
+            <Input type="text" placeHolder="selling_price" icon="selling_price" 
+            defaultValue={state.inputs?.selling_price ?? ""}
+            onChange={() => setField({...field, selling_price: {isEdited: true}})}
+            name={'selling_price'} />
+            
+            {state?.errors?.selling_price && <span className="field_error">{state?.errors?.selling_price}</span>}
+            
+            <label>Stock</label>
+            <Input type="text" placeHolder="Stock" icon="boxes" 
+            defaultValue={state.inputs?.stock ?? ""}
+            onChange={() => setField({...field, stock: {isEdited: true}})}
+            name={'stock'} />
+            
+            {state?.errors?.stock && <span className="field_error">{state?.errors?.stock}</span>}
+                
+            {state?.errors?.error && <span className="field_error">{state?.errors?.error}</span>}
+
+            {state?.message && <span style={{color: 'green', marginTop: '8px'}}>{state?.message}</span>}
+
+            {/* send form */}
+            <Button role="submit" type="secondary">
+                {isPending && <OvalLoader/>}   
+                {isPending ? 'Guardando...' : 'Guardar Producto'}
+            </Button>
+        </Form>
+    )
+    
+}
--- packages/frontend/inventory-online-app/app/(store)/store/products/add/page.jsx
+++ packages/frontend/inventory-online-app/app/(store)/store/products/add/page.jsx
@@ -1,57 +1,16 @@
-'use client'
-import { Form } from '@/app/ui/form/form/form'
-import { Input } from '@/app/ui/form/input/input'
-import { Button } from '@/app/ui/utils/button/buttons'
+import { buildQueryParams } from '@/app/utils/buildQueryParams'
 import Route from '@/app/ui/routesLinks/routes'
-import styles from './input.module.css'
-import { useState } from 'react'
-import { errorHandler } from '@/app/errors/fetchDataErrorHandler'
-import fetchData from '@/app/utils/fetchData'
-const NEXT_PUBLIC_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://127.0.0.1'
+import AddProductForm from '@/app/(store)/store/products/_components/add/addProductForm'
 
-export default function AddProduct() {
-    const [message, setMessage] = useState(null)
-    const [errors, setErrors] = useState(null)
-    const [name, setName] = useState('')
-    const [barcode, setBarcode] = useState('')
-    const [purchasePrice, setPurchasePrice] = useState('')
-    const [sellingPrice, setSellingPrice] = useState('')
-    const [stock, setStock] = useState('')
 
-    const addProduct = async () => {
-        if (!name, !barcode, !purchasePrice, !sellingPrice, !stock) return
-
-        return await errorHandler( async () => {
-            setErrors(null)
-            const endpoint = '/api/products'
-            const url = `${NEXT_PUBLIC_API_BASE_URL}${endpoint}`
-            const data = await fetchData(url, 'POST', { name, barcode, purchasePrice, sellingPrice, stock })
-            if (data) {
-                setMessage('Producto agregado con éxito')
-                setName('')
-                setBarcode('')
-                setPurchasePrice('')
-                setSellingPrice('')
-                setStock('')
-                setTimeout(() => {
-                    setMessage(null)
-                }, 2000)
-            }
-        }, null, setErrors, 'Hubo un error al agregar el producto')
-    }
+export default async function AddProduct({searchParams}) {
+    const urlParams = await searchParams
+    const queryString = buildQueryParams(urlParams, ['page', 'data'])
 
     return (
-        <>  
-            <Route path='products' endpoints={['default', 'add']} />
-            <Form className={`${styles.form} shadow`}  onSubmit={(e) => {e.preventDefault(); addProduct();}}>
-                <Input type="text" placeHolder="Nombre del producto" icon="product" 
-                   onChange={(e) => setName(e.target.value)} value={name} name={'name'} capitalize={true}/>
-                <Input type="text" placeHolder="Código de barras" icon="barcode" 
-                   onChange={(e) => setBarcode(e.target.value)} value={barcode} name={'barcode'}/>
-
-            </Form>
-        </>
-    )
-
+        <>
+            <Route path='products' endpoints={['default', 'add']} queryString={queryString}/> 
+            <AddProductForm/>
+        </>          
+   )
 }
-
