6959f4500bb52644ceef02f67b7db8d6559d9099
pedro092692 <pedro092692@gmail.com>
2026-04-08 10:22:11 +0200

    refactor: add placeHolder on selling_price inpunt on add  product form

--- packages/frontend/inventory-online-app/app/(store)/store/products/_components/add/addProductForm.jsx
+++ packages/frontend/inventory-online-app/app/(store)/store/products/_components/add/addProductForm.jsx
@@ -70,7 +70,7 @@ export default function AddProductForm() {
             {state?.errors?.purchase_price && <span className="field_error">{state?.errors?.purchase_price}</span>}
 
             <label>Precio de venta $</label>
-            <Input type="text" placeHolder="selling_price" icon="selling_price" 
+            <Input type="text" placeHolder="Precio De Venta $" icon="selling_price" 
             defaultValue={state.inputs?.selling_price ?? ""}
             onChange={() => setField({...field, selling_price: {isEdited: true}})}
             name={'selling_price'} />
