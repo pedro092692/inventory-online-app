13abbba31c92a35a49930775f185688d36f5dc82
pedro092692 <pedro092692@gmail.com>
2026-04-10 09:58:51 +0200

    refactor: delete getUser util function

--- packages/frontend/inventory-online-app/app/utils/getUser.js
+++
@@ -1,9 +0,0 @@
-import getData from '@/app/utils/fetchData'
-const NEXT_PUBLIC_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://127.0.0.1'
-
-export async function getUser() {
-    const currentUser = getData(`${NEXT_PUBLIC_API_BASE_URL}/api/security/current-user`, 'GET')
-    return currentUser
-}
-
- 
