2a720e170e5d1560e1d20dba597a0a56838a2e98
pedro092692 <pedro092692@gmail.com>
2026-06-30 20:43:01 +0200

    feat: create totalInfo component

--- packages/frontend/inventory-online-app/app/(store)/store/sell/_components/totalInfo/totalInfo.jsx
+++ packages/frontend/inventory-online-app/app/(store)/store/sell/_components/totalInfo/totalInfo.jsx
@@ -1,21 +1,61 @@
+import styles from './totalInfo.module.css'
 
 export default function totaInfo({total={total_usd: 0, total_bs: 0}, totalPaidUSD=0, exchangeRate=1, remainingToPayUSD=0, changeDueUSD=0}) {
     return (
-        <div>
-            <p>Total Factura: {total?.total_usd.toFixed(2) || 0} $ / {total?.total_bs.toFixed(2) || 0} Bs</p>
-            {totalPaidUSD > 0 && (
-                <p>Total Abonado: {totalPaidUSD.toFixed(2) || 0} $ / {(totalPaidUSD * exchangeRate || 0).toFixed(2)} Bs</p>
-            )}
+        <div className={`${styles.infoContainer} shadow-bottom-sm`}>
+            <div className={`${styles.payInfo} shadow`}>
+                <h2>Total A Pagar:</h2>
+                <p className={styles.total}>
+                    {new Intl.NumberFormat('en-US').format(total?.total_usd.toFixed(2) || 0)} $
+                </p>
+                <div className='divider'></div>
+                <p className={styles.total}>
+                    {new Intl.NumberFormat('es-VE').format(total?.total_bs.toFixed(2) || 0)} Bs
+                </p>
+            </div>
+
+             {
+                totalPaidUSD !=0 && remainingToPayUSD > 0.01 && (
+                    <div className={`${styles.payInfo} shadow`}>
+                        <h2>Por pagar:</h2>
+                        <p className={styles.totalToPaid}>
+                            {new Intl.NumberFormat('en-US').format(remainingToPayUSD.toFixed(2) || 0)} $ 
+                        </p>
+                        <div className='divider'></div>
+                        <p className={styles.totalToPaid}>
+                            {new Intl.NumberFormat('en-US').format((remainingToPayUSD * exchangeRate).toFixed(2))} Bs
+                        </p>
+                    </div>
+                )
+            }
             
-            <p>Resta por pagar: {remainingToPayUSD.toFixed(2)} $ /
-                {(remainingToPayUSD * exchangeRate).toFixed(2)} Bs
-            </p>
+            {
+                totalPaidUSD > 0 && (
+                    <div className={`${styles.payInfo} shadow`}>
+                        <h2>Total Abonado:</h2>
+                        <p className={styles.totalPaid}>
+                            {new Intl.NumberFormat('en-US').format(totalPaidUSD.toFixed(2) || 0)} $ 
+                        </p>
+                        <div className='divider'></div>
+                        <p className={styles.totalPaid}>
+                            {new Intl.NumberFormat('en-US').format((totalPaidUSD * exchangeRate || 0).toFixed(2))} Bs
+                        </p>
+                    </div>
+                )
+            }
+            
             {
                 changeDueUSD > 0 && (
-                    <p style={{color: 'green', fontWeight: 'bold'}}> 
-                        Cambio (Vuelto): {changeDueUSD.toFixed(2) || 0} $ / 
-                        {(changeDueUSD * exchangeRate || 0).toFixed(2)} Bs
-                    </p>
+                    <div className={`${styles.payInfo} shadow`}>
+                        <h2>Cambio (Vuelto):</h2>
+                        <p className={styles.totalPaidChange}>
+                            {new Intl.NumberFormat('en-US').format(changeDueUSD.toFixed(2) || 0)} $ 
+                        </p>
+                        <div className='divider'></div>
+                        <p className={styles.totalPaidChange}>
+                            {new Intl.NumberFormat('en-US').format((changeDueUSD * exchangeRate || 0).toFixed(2))} Bs
+                        </p>
+                    </div>
                 )
             }
         </div>
