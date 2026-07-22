cc73102d5b711f45ecad34864b65c37b92b76231
pedro092692 <pedro092692@gmail.com>
2026-07-22 16:57:21 +0200

    feat: create layout for report component

--- packages/frontend/inventory-online-app/app/(store)/store/reports/page.jsx
+++ packages/frontend/inventory-online-app/app/(store)/store/reports/page.jsx
@@ -3,7 +3,7 @@ import { Title } from "@/app/ui/dashboard/title/title"
 export default function Reports() {
     return (
         <>
-            <Title title="Reportes" icon='report'/>
+            <p>Reports Here :P</p>
         </>
     )
 }
