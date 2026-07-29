8fd67e9823cd740818b071552279b2ef3b72f3dd
pedro092692 <pedro092692@gmail.com>
2026-07-29 19:22:55 +0200

    feat: create sales detail page

---
+++ packages/frontend/inventory-online-app/app/(store)/store/reports/sales/details/page.jsx
@@ -0,0 +1,19 @@
+
+import FormSkeleton from '@/app/ui/skeleton/form/formSkeleton'
+import { Suspense } from 'react'
+import {Container} from '@/app/ui/utils/container'
+
+
+export default function PatternsReport() {
+    return (
+         <Suspense key={'charts'} fallback={<FormSkeleton nFields={1}/>} >
+            <Container
+                padding={'16px'}
+                width={'100%'}
+                gap={'24px'}
+            >   
+                Details
+            </Container>
+        </Suspense>
+    )
+}
